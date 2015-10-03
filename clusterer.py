254c64bb0a6aaab728dbfa178518a5a43d0166a5
Theodore DeRego <tderego94@gmail.com>
2015-10-02 15:56:42 -1000

    Fixed clusterer

--- clusterer.py
+++ clusterer.py
@@ -16,10 +16,14 @@ def main(args):
 
     contig_sim = [] # Contig similarity [(contig_a, contig_b, num_barcodes_in_common)]
     contigs = set() # Set of all contigs
+    got_column_labels = False
 
     for line in sys.stdin:
         # Skip commented lines
         if line.startswith("#"):
+            continue
+        if not got_column_labels:
+            got_column_labels = True
             continue
         contig_a, contig_b, barcodes_in_common = line.strip().split("\t")
         contig_sim.append((contig_a, contig_b, int(barcodes_in_common)))
@@ -42,9 +46,11 @@ def main(args):
     # Cluster all the things
     while len(clusters) > desired_cluster_count:
         # Get next highest connection
-        biggest = 0
-        for c in range(1, len(contig_sim)):
-            if contig_sim[c][2] > contig_sim[biggest][2]:
+        biggest = None
+        for c in range(0, len(contig_sim)):
+            cluster_a = contig_clusters[contig_sim[c][0]]
+            cluster_b = contig_clusters[contig_sim[c][1]]
+            if (biggest == None or contig_sim[c][2] > contig_sim[biggest][2]) and cluster_a != cluster_b: 
                 biggest = c
         contig_a = contig_sim[biggest][0]
         contig_b = contig_sim[biggest][1]
