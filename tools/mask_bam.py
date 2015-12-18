7ee75efd3f916aa2c0c6755edc75a1fc67acf7aa
ken <nagatak@hawaii.edu>
2015-12-18 13:45:25 -1000

    changed output to stdout

--- tools/mask_bam.py
+++ tools/mask_bam.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 import argparse
+import sys
 
 #If readcounts exceed a specified threshold,
 #this program will mark areas to be masked.
-
+#run with:
+#python mask_bam.py --minReadCount -l --maxReadCount -u -readCountFile -f > out
 
 # find the average of 10 read counts takes a list an arg.
 def findAverage(l):
@@ -88,56 +90,52 @@ def Main():
                             "input file", type=str, required = True)
         args = parser.parse_args()
 
-        #opens output file maskOutput.bed
-        with open('maskOutput.bed', 'a') as outFile:
-            #reads in input file
-            with open(args.readCountFile) as input:
-                count = 0
+        #reads in input file
+        with open(args.readCountFile, 'r') as input:
+            count = 0
 
-                #strips and split input line and stores into list 
-                for line in input:
-                    fields = line.strip().split()
-                    if len(fields) < 4:
-                        continue
-                    reads.append(fields)
-                    count = count + 1
+            #strips and split input line and stores into list 
+            for line in input:
+                fields = line.strip().split()
+                if len(fields) < 4:
+                    continue
+                reads.append(fields)
+                count = count + 1
 
-                    #print fields[3]
-                    #read above max threshold has been found, 
-                    if int(fields[3]) >= args.maxReadCount \
-                            and pastLowerFound == False:
-                        #check for lower bound the has been read in    
-                        arr = pastLowerBound(reads, args.minReadCount)
-                        if len >= 10:
-                            mid = 5
-                        else:
-                            mid = 0
-                        pastLowerFound = True
-                        count = 0
-                        outFile.write("%s\t%s" %(arr[0], (int(arr[1]) + mid)))
-                    #past lower bound has been found    
-                    elif pastLowerFound == True and count == 10:
-                        #check new values read in for lower bound
-                        arr, futureLowerFound = futureLowerBound(reads, args.minReadCount)
-                        if futureLowerFound == True:
-                            outFile.write("\t%d\tmask\n" %(int(arr[1]) + 5))
-                            count = 0
-                        else:
-                            break
-                    #both past and future lowerbounds have been found reset bools and list        
-                    if pastLowerFound == True and futureLowerFound == True:
-                        reads = []
-                        pastLowerFound = False
-                        futureLowerFound = False
-                if pastLowerFound == True and futureLowerFound == False:
+                #read above max threshold has been found, 
+                if int(fields[3]) >= args.maxReadCount \
+                        and pastLowerFound == False:
+                    #check for lower bound the has been read in    
+                    arr = pastLowerBound(reads, args.minReadCount)
+                    if len(reads) >= 10:
+                        mid = 5
+                    else:
+                        mid = 0
+                    pastLowerFound = True
+                    count = 0
+                    sys.stdout.write("%s\t%s" %(arr[0], (int(arr[1]) + mid)))
+                    sys.stdout.flush()
+                #past lower bound has been found    
+                elif pastLowerFound == True and count == 10:
+                    #check new values read in for lower bound
                     arr, futureLowerFound = futureLowerBound(reads, args.minReadCount)
-                    mid = len(reads)/2
-                    outFile.write("\t%d\tmask\n" %(int(arr[1]) + mid))
+                    if futureLowerFound == True:
+                        sys.stdout.write("\t%d\tmask\n" %(int(arr[1]) + 5))
+                        count = 0
+                    else:
+                        break
+                #both past and future lowerbounds have been found reset bools and list        
+                if pastLowerFound == True and futureLowerFound == True:
+                    reads[:] = []
+                    pastLowerFound = False
+                    futureLowerFound = False
+            if pastLowerFound == True and futureLowerFound == False:
+                arr, futureLowerFound = futureLowerBound(reads, args.minReadCount)
+                mid = len(reads)/2
+                sys.stdout.write("\t%d\tmask\n" %(int(arr[1]) + mid))
 
-            #input file closed
-            input.close()
-        #output file closed
-        outFile.close()
+        #input file closed
+        input.close()
 
 ###############################################################################
 if __name__ == '__main__':
