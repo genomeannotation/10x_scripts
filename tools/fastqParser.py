1d29187207da543cc15d7d19ce7b9df03559cf3e
ken <nagatak@hawaii.edu>
2016-01-22 15:04:46 -1000

    now working, fixed syntax and mkdir errors

--- tools/fastqParser.py
+++ tools/fastqParser.py
@@ -1,21 +1,32 @@
 #! /usr/bin/env/ python2.7
 #program parses a fastq file and separates into smaller fastq files by barcode
+#run with python fastqParser.py -p potentialReads -f fastqFile
+#output stores files in ~/fastqFiles/
 
 import argparse
 import sys
+import os
 
-#function makes a file w/name passed in, 
+#function creates directory if none exists, makes a file w/name passed in,  
 #if file already exists then does nothing
 def makefile(l):
-    with open('%s.fastq' % l, 'a') as myfile:
+    filename = '~/fastqFiles/%s.fastq' % l 
+    if not os.path.exists('~/fastqFiles/'):
+        os.makedirs('~/fastqFiles/', 0777)
+    with open(filename, 'a') as myfile:
         myfile.close()
 
 #function writes to fasta file, creates file if does not exist, appends
 #to file if it does exist
 def writeToFile(b, f):
-    with open('%s.fastq' % b, 'a') as myfile:
-        myfile.write('/t'.join(f) + '\n')
-    myfile.close()
+    filename = '~/fastqFiles/%s.fastq' % b
+    if not os.path.exists('~/fastqFiles/'):
+        os.makedirs('~/fastqFiles/', 0777)
+    for i in range(len(f)):
+        temp = f[i]
+        with open(filename, 'a') as myfile:
+            myfile.write('/t'.join(temp) + '\n')
+        myfile.close()
 
 #removes first 3 and last 2 chars from file name
 def cleanFileName(f):
@@ -27,7 +38,8 @@ def cleanBarcode(b):
 
 def main():
     fields = []
-    barcode = []
+    temp = []
+    foundSequence = False
     count = 0
 
     #arg parse stuff
@@ -51,15 +63,28 @@ def main():
     if args.fastqInput:
         with open(args.fastqInput, 'r') as fqin:
             for line in fqin:
-                fields = line.strip().split('\t')
-                if len(fields) > 1:
-                    for i in range (len(fields)):
-                        #search for fields that start with BX:
-                        if fields[i].startswith('BX:'):
-                            barcode = fields
-                            writeToFile(str(cleanBarcode(barcode[i])), fields)
-                    barcode[:] = []
-                    fields[:] = []
+                fields = line.strip().split()
+                
+                #checks for start  sequence
+                if len(fields) > 0 and fields[0].startswith('@'):
+                    foundSequence = True
+                    
+                    #checks for barcode, if exists barcode will be used as
+                    #filename, esle filename set to discard
+                    if len(fields) > 1 and fields[1].startswith('BX:'): 
+                        barcode = str(cleanBarcode(fields[1]))
+                    else:
+                        barcode = 'discard'
+                
+                if len(fields) > 0 and foundSequence == True:
+                    temp.append(fields)
+                
+                #writes 4 lines to file
+                if len(temp) > 3 and foundSequence == True:
+                    writeToFile(barcode, temp)
+                    temp[:] = []
+                    del barcode
+                    foundSequence = False
 
 ###############################################################################
 if __name__ == '__main__':
